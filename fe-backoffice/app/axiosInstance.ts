0140879d987db96546a649cf37569d008c9999f7
mane936 <53373350+mane936@users.noreply.github.com>
2023-08-16 19:27:32 +0200

    doc

--- fe-backoffice/app/axiosInstance.ts
+++ fe-backoffice/app/axiosInstance.ts
@@ -1,8 +1,6 @@
 import { getIsAuth } from "@/utils/getIsAuth";
 import axios from "axios";
 
-import Cookies from "js-cookie";
-
 export const instance = axios.create({
   baseURL: "http://localhost:8080", // ONLY FOR DEV: comment this for production
   headers: {
