306b195e77e770b313acc93ff0e67701c0b88b55
mane936 <53373350+mane936@users.noreply.github.com>
2023-09-23 10:24:18 +0200

    ip

--- be-backoffice-alpha/src/utils/domains.ts
+++ be-backoffice-alpha/src/utils/domains.ts
@@ -1,9 +1,13 @@
 import { Request } from "express";
 
 export const getDomain = (req: Request) => {
-  const ip = req.headers["x-forwarded-for"] as string;
+  let ip = req.headers["x-forwarded-for"] as string;
 
-  console.log("ip: ", ip);
+  if (typeof ip === "string") {
+  } else {
+    ip = ip[0];
+  }
+  console.log("ip: ", `"${ip}"`);
 
   return ip;
 };
