6eb1f135e73ae20ba3f66a5dc4284068f894d3ce
mane936 <53373350+mane936@users.noreply.github.com>
2023-09-23 09:49:09 +0200

    error

--- be-backoffice-alpha/src/utils/domains.ts
+++ be-backoffice-alpha/src/utils/domains.ts
@@ -1,5 +1,4 @@
 import { Request } from "express";
-import { UsingJoinTableOnlyOnOneSideAllowedError } from "typeorm";
 
 export const getDomain = (req: Request) => {
   const ip = req.headers["x-forwarded-for"] as string;
